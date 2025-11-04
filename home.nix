b098610552e571d1e8517cd32e4cd916ce818d54
Arik Rahman <40479733+ArikRahman@users.noreply.github.com>
2025-11-04 13:50:34 -0600

    Track flake.nix and home.nix

---
+++ home.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  home.username = "arik";
+  home.homeDirectory = "/Users/arik";
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+  # example packages
+  home.packages = with pkgs; [
+    nixfmt-rfc-style
+    cowsay
+    nil
+    nixd
+    lazygit
+  ];
+}
