b9d8e7b252ef0a4ab414e9fd733bc0a98461c968
arik <arik@tamu.edu>
2025-12-22 21:46:44 -0600

    New nixos setup on nitro laptop
    Init

--- flake.nix
+++ flake.nix
@@ -1,107 +1,22 @@
 {
-  description = "Arik's Flake.nix";
-
-  # Flake inputs
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # Stable Nixpkgs (use 0.1 for unstable)
+  description = "Arik's NixOS Flake";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nix-doom-emacs-unstraightened.url = "github:marienz/nix-doom-emacs-unstraightened";
-    nix-doom-emacs-unstraightened.inputs.nixpkgs.follows = "";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    zen-browser.url = "github:youwen5/zen-browser-flake";
     home-manager = {
-      url = "github:nix-community/home-manager";
-
+      url = "github:nix-community/home-manager"; # Will track the latest matching nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  # Flake outputs
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      # The systems supported for this flake's outputs
-      supportedSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.arik = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; }; # IMPORTANT: This passes 'inputs' to configuration.nix
+      modules = [
+        ./configuration.nix
       ];
-
-      # Helper for providing system-specific attributes
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            inherit system;
-            # Provides a system-specific, configured Nixpkgs
-            pkgs = import inputs.nixpkgs {
-              inherit system;
-              # Enable using unfree packages
-              config.allowUnfree = true;
-            };
-          }
-        );
-    in
-    {
-      # Development environments output by this flake
-
-      # To activate the default environment:
-      # nix develop
-      # Or if you use direnv:
-      # direnv allow
-      devShells = forEachSupportedSystem (
-        { pkgs, system }:
-        {
-          # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
-          default = pkgs.mkShellNoCC {
-            # The Nix packages provided in the environment
-            packages = with pkgs; [
-              # Add the flake's formatter to your project's environment
-              self.formatter.${system}
-
-              # Other packages
-              ponysay
-            ];
-
-            # Set any environment variables for your development environment
-            env = { };
-
-            # Add any shell logic you want executed when the environment is activated
-            shellHook = "";
-          };
-        }
-      );
-
-      # Nix formatter
-
-      # This applies the formatter that follows RFC 166, which defines a standard format:
-      # https://github.com/NixOS/rfcs/pull/166
-
-      # To format all Nix files:
-      # git ls-files -z '*.nix' | xargs -0 -r nix fmt
-      # To check formatting:
-      # git ls-files -z '*.nix' | xargs -0 -r nix develop --command nixfmt --check
-      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
-
-      # --- Add this block below ---
-      homeConfigurations = {
-        "arik" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs { system = "aarch64-darwin"; };
-          modules = [
-            inputs.nix-doom-emacs-unstraightened.homeModule
-            ./home.nix
-          ];
-          extraSpecialArgs = { inherit inputs; };
-
-        };
-      };
-      # --- End of new block ---
-
     };
+  };
 }
